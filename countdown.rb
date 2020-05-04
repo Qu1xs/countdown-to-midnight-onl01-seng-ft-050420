3cf956e9463f6a7f7e5960f52f0be5557e35851a
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 11:31:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+i=0
+  while 1 > number
+    puts "#{number} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  
 end
